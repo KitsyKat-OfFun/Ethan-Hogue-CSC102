fd0557a36dec6797ef93fcc871f1037ed77f3db6
KitsyKat-OfFun <133930918+KitsyKat-OfFun@users.noreply.github.com>
2024-11-24 22:13:08 -0700

    Add files via upload

---
+++ w11AudioScript.js
@@ -0,0 +1,34 @@
+//This function will add sound to our page.
+function addSound()
+{
+    //Create an audio element, and get the play and pause buttons.
+    var soundElement = document.createElement("audio");
+    var btnPlay = document.getElementById("btnPlay");
+    var btnPause = document.getElementById("btnPause");
+
+    //Set the sound ID.
+    soundElement.setAttribute("id", "audioSound");
+
+    //Set the sound file that'll be played.
+    soundElement.setAttribute("src", "MMWorld3ExtraLife.wav");
+
+    document.body.appendChild(soundElement);
+
+    //Show the play and pause buttons.
+    btnPlay.hidden = false;
+    btnPause.hidden = false;
+}
+
+function playSound()
+{
+    var audio = document.getElementById("audioSound");
+    audio.play();
+    console.log("sound playing");
+}
+
+function pauseSound()
+{
+    var audio = document.getElementById("audioSound");
+    audio.pause();
+    console.log("sound paused")
+}
